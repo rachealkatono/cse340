84be5596b6d27da16417592b2be038bf7896f4eb
rachealkatono <you@example.com>
2025-03-11 00:18:20 +0300

    week1

--- server.js
+++ server.js
@@ -6,21 +6,42 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
 const port = process.env.PORT
 const host = process.env.HOST
+
+/* ***********************
+ * typical default file name
+ *************************/
+/* ***********************
+* index
+*************************/
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout") // not at views root
+
 
 /* ***********************
  * Log statement to confirm server operation
